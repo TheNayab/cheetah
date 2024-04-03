5fa1fa0faa71b46cbc86e8cd4c9c27c86a80a737
Nayab <nayab8609@gmail.com>
2024-04-03 12:08:15 -0700

    sec

--- Server.js
+++ Server.js
@@ -1,8 +1,5 @@
 const app = require("./App.js");
 const connectToDb = require("./configure/db.js");
-const dotenv = require("dotenv");
-
-dotenv.config({ path: "../backend/configure/config.env" });
 
 connectToDb();
 
