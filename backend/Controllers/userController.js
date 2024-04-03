94e790fd8dcccf390de5600251abea44e3cdfafd
Nayab <nayab8609@gmail.com>
2024-04-03 14:44:24 -0700

    sdfgs

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -76,7 +76,7 @@ router.post(
                   res
                     .status(200)
                     .cookie("token", authToken, {
-                      expires: "30d",
+                      expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
                       httpOnly: true,
                     })
                     .json({
@@ -148,7 +148,7 @@ router.post(
               return res
                 .status(200)
                 .cookie("token", authToken, {
-                  expires: "30d",
+                  expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
                   httpOnly: true,
                 })
                 .json({
